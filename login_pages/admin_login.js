d9a1ce628ad0dca76d58dfac8f832e481d6bb15f
eugene44-hub <eugeneishado44@gmail.com>
2022-01-23 04:50:44 +0100

    fixed admin auth

--- login_pages/admin_login.js
+++ login_pages/admin_login.js
@@ -1,8 +1,13 @@
+import { signInUser } from "../admin/firebase.js"
 const form = document.querySelector("form")
 
 form.addEventListener("submit", e => {
+    e.preventDefault()
+
     const email = document.querySelector("#email").value
     const password = document.querySelector("#password").value
-    e.preventDefault()
-    errHandler(email, password, "/admin/admin.html")
+    console.log(email)
+    email && password ? signInUser(email, password, "../admin/admin.html") : swal({ title: "Blank fields", icon: "error", text: "fill blank fields" })
+
+    // errHandler(email, password, "/patient/patient.html")
 })
